26728bd539cd53df7b1ec1c61661f3e20cc43e98
Zaur Giyasov <zaurusnnov@icloud.com>
2018-03-31 13:38:30 +0300

    Assigment 1 - p.3 Add Deck*

--- CardGame/ViewController.m
+++ CardGame/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "Deck.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+@property (nonatomic) Deck *deck;
 
 
 @end
